0ef4c059984f6bf9802fea8851f20dd2285439a7
armin almuete <armin.almuete@gmail.com>
2017-10-18 14:33:18 +0800

    Modified and added wild card to check if img src have random html attributes in the middle of the img src

--- app/code/Acidgreen/ImageLazyLoader/Plugin/ResponsePlugin.php
+++ app/code/Acidgreen/ImageLazyLoader/Plugin/ResponsePlugin.php
@@ -7,7 +7,7 @@ class ResponsePlugin {
     public function aroundsendContent(\Zend\Http\PhpEnvironment\Response $response, callable $proceed) {
         $content = $response->getContent();
         
-        $content = preg_replace("/(?<=img)(.*)src/Ui", "$1data-src", $content);
+        $content = preg_replace("/(<img\s*.*?)src/sm", "$1data-src", $content);
 
         $response->setContent($content);
         return $proceed();
